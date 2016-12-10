f4889352e5d42ec8a8b21072df86052ff884cc35
sweetpotato <sweetpotato@users.noreply.github.com>
2016-12-10 17:29:24 +0900

    4oty: replace xargs with for-loop

--- 4oty/4otytrack.sh
+++ 4oty/4otytrack.sh
@@ -2,12 +2,12 @@
 source "${0%/*}/4otyrc"
 
 function get-untracked-users() {
-	cat "$DATA"/*.tsv |\
-	grep '^neighbor'  |\
-	awk '{print $3;}' |\
-	sort              |\
-	uniq              |\
-	xargs -n 1 bash -c '[ -f "$0/${1##*/}.tsv" ] || echo "$1"' "$DATA"
+	local users=($(\
+		grep -hR '^neighbor' "$DATA" | cut -s -f 3 | sort | uniq \
+	))
+	for user in "${users[@]}" ; do
+		[ -f "$DATA/${user##*/}.tsv" ] || echo "$user"
+	done
 }
 
 untracked=($(get-untracked-users))
