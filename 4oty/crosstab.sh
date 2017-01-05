683929fd48703040ea15d9f40718a48439c1d7a3
sweetpotato <sweetpotato@users.noreply.github.com>
2017-01-06 03:49:42 +0900

    4oty: tidy-up crosstab

--- 4oty/crosstab.sh
+++ 4oty/crosstab.sh
@@ -4,7 +4,7 @@ set -ux
 source "${0%/*}/4otyrc"
 
 (
-	grep -c ^newbook_ data/*.tsv | awk -F: '{print $1,"a",$2;}'
-	grep -c ^contbook data/*.tsv | awk -F: '{print $1,"b",$2;}'
-) | sort -k1,2 | xargs -n 6 echo | awk '{print $3,$6;}' | sort -n -r | uniq -c |\
-awk -f print_crosstab.awk
+	find-data | xargs grep -c ^newbook_ | awk -F: '{print $1,"a",$2}'
+	find-data | xargs grep -c ^contbook | awk -F: '{print $1,"b",$2}'
+) | sort -k1,2 | xargs -n 6 echo | awk '{print $3,$6}' | sort -n -r |\
+    uniq -c | awk -v OFS=$'\t' -f print_crosstab.awk
--- 4oty/print_crosstab.awk
+++ 4oty/print_crosstab.awk
@@ -1,7 +1,6 @@
 function z(x){return x?x:0}
 {m[$2,$3]=$1}
 END{
-	OFS="\t";
 	print "cont","new";
 	print "",0,1,2,3,4,5;
 	for(i=0;i<=5;i++){
