6a389f134de65b40a008932e79a8d10b673388cd
sweetpotato <sweetpotato@users.noreply.github.com>
2017-01-06 04:05:54 +0900

    4oty: crosstab by join

--- 4oty/crosstab.sh
+++ 4oty/crosstab.sh
@@ -3,8 +3,8 @@ export LANG=C LC_ALL=C
 set -ux
 source "${0%/*}/4otyrc"
 
-(
-	find-data | xargs grep -c ^newbook_ | awk -F: '{print $1,"a",$2}'
-	find-data | xargs grep -c ^contbook | awk -F: '{print $1,"b",$2}'
-) | sort -k1,2 | xargs -n 6 echo | awk '{print $3,$6}' | sort -n -r |\
-    uniq -c | awk -v OFS=$'\t' -f print_crosstab.awk
+join -t: \
+  <(find-data | xargs grep -c ^newbook_ | sort -t: -k1,1) \
+  <(find-data | xargs grep -c ^contbook | sort -t: -k1,1) |\
+awk -F: '{print $2,$3;}' | sort -n -r | uniq -c |\
+awk -v OFS=$'\t' -f print_crosstab.awk
