f6d5002ff2f65e0380552560b3ea2df659ffdb88
sweetpotato <sweetpotato@users.noreply.github.com>
2017-01-06 15:11:02 +0900

    4oty: introduce function awk-otsv

--- 4oty/publisher.sh
+++ 4oty/publisher.sh
@@ -27,9 +27,9 @@ if [ "$sub" = yes ] ; then
 	find-data | xargs grep -h "^$target" | cut -f3 | sed 's!.*/dp/!!' |\
 	"${0%/*}/isbn.pl" | cut -f1,2 | count |\
 	awk '$3!="-"{print $2,$1,$3}' | sort -n -r |\
-	awk -v OFS=$'\t' '{print $1,$3,$2}'
+	awk-otsv '{print $1,$3,$2}'
 else
 	find-data | xargs grep -h "^$target" | cut -f3 | sed 's!.*/dp/!!' |\
 	"${0%/*}/isbn.pl" | cut -f1 | count | sort -n -r |\
-	awk -v OFS=$'\t' '{print $2,"","",$1}'
+	awk-otsv '{print $2,"","",$1}'
 fi
