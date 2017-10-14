1759acacabbc65c4ba853ae120bf7af777c11369
sweetpotato <sweetpotato@users.noreply.github.com>
2017-10-15 01:54:34 +0900

    wccheck: genepixiv 301 moved permanently

--- wccheck/genepixiv/run.sh
+++ wccheck/genepixiv/run.sh
@@ -3,7 +3,7 @@ export LANG=C LC_ALL=C
 set -xueo pipefail
 source "${0%/*}"/../wccheck.rc
 
-url=http://genepixiv.pr.pixiv.net/
+url=https://genepixiv.pr-pixiv.net/
 fetch_and_parse_today_data "$url"
 diff_latest_two_data_txt
 
