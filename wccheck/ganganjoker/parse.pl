23fe120de05f6b17d3016f2b4dcb99281f7e31ac
sweetpotato <sweetpotato@users.noreply.github.com>
2018-11-04 10:09:41 +0900

    wccheck: ganganjoker fix regex

--- wccheck/ganganjoker/parse.pl
+++ wccheck/ganganjoker/parse.pl
@@ -10,7 +10,7 @@ $param{base} = 'http://www.jp.square-enix.com/magazine/joker/series/';
 $param{re_pre} = qr|<div class="series_box">(.*?)</div>|s;
 $param{re_loop} = qr{
 	<a[ ]href="(?'link'[^\x{22}]*)">
-	<img[ ][^>]*[ ]alt="(?'title'[^\x{22}]*)"[ ][ ]/>
+	<img[ ][^>]*[ ]alt="(?'title'[^\x{22}]*)"[^>]*>
 }sx;
 
 WCCheck::parse(%param);
